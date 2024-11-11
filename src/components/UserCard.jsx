c86c93f74f9a4d230ec50fc77c527ff232acd9a9
subrata <subrata.de@pasdgtal.com>
2024-11-11 17:48:46 +0530

    finished

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,22 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaLocationDot } from "react-icons/fa6";
+import { LuContact } from 'react-icons/lu';
+import { MdEmail, MdPassword } from 'react-icons/md';
+import { getFullName } from '../consts';
 
-const UserCard = ({ name, image }) => {
+const UserCard = ({ user }) => {
+    const [fieldName, setFieldName] = useState("");
+
     return (
-        <div className='w-[300px] h-[300px] bg-red-500 shadow rounded relative'>
+        <div className='w-[300px] h-[330px] bg-red-500 shadow rounded relative'>
             <div className="w-full h-20 border-b"></div>
             <img
                 className='absolute top-6 left-[100px] rounded-full p-1 bg-white border-black'
                 width={100}
-                src={image}
-                alt={name} />
+                src={user?.picture?.large}
+                alt={getFullName(...Object.values(user.name))} />
 
-            <div className='w-full h-[220px] flex flex-col justify-evenly items-center'>
+            <div className='w-full min-h-[200px] max-h-[250px] flex flex-col justify-evenly items-center mt-10'>
                 <div className="w-full flex flex-col justify-end text-gray-50 items-center">
                     Hi, My name is
-                    <h2 className='text-xl font-bold text-white'>{name}</h2>
+                    <h2 className='text-xl font-bold text-white'>{getFullName(...Object.values(user.name))}</h2>
+                    {
+                        fieldName == "loc" && <p>{user?.location?.coordinates?.latitude}, {user?.location?.coordinates?.longitude}</p>
+                    }
+
+                    {
+                        fieldName == "email" && <p>{user.email}</p>
+                    }
+
+                    {
+                        fieldName == "contact" && <p>{user.phone}</p>
+                    }
+
+                    {
+                        fieldName == "pass" && <p>{user.login.password}</p>
+                    }
                 </div>
                 <div className="w-full flex justify-center items-center px-10">
-                    <div className="w-full bg-[#0003] rounded ">&nbsp;</div>
+                    <div className="w-full rounded p-3 flex justify-between items-center ">
+                        <button
+                            onMouseOver={() => setFieldName('loc')}
+                            className={`p-3 bg-[#0f03] rounded-full ${fieldName == "loc" && "bg-[#0f0]"}`}
+                        >
+                            <FaLocationDot
+                                size={20}
+                                color='white'
+                            />
+                        </button>
+
+
+                        <button
+                            onMouseOver={() => setFieldName('email')}
+                            className={`p-3 bg-[#0f03] rounded-full ${fieldName == "email" && "bg-[#0f0]"}`}
+                        >
+                            <MdEmail
+                                size={20}
+                                color='white'
+                            />
+                        </button>
+
+
+                        <button
+                            onMouseOver={() => setFieldName('contact')}
+                            className={`p-3 bg-[#0f03] rounded-full ${fieldName == "contact" && "bg-[#0f0]"}`}
+                        >
+                            <LuContact
+                                size={20}
+                                color='white'
+                            />
+                        </button>
+
+                        <button
+                            onMouseOver={() => setFieldName('pass')}
+                            className={`p-3 bg-[#0f03] rounded-full ${fieldName == "pass" && "bg-[#0f0]"}`}
+                        >
+                            <MdPassword
+                                size={20}
+                                color='white'
+                            />
+                        </button>
+                    </div>
                 </div>
 
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,29 +5,73 @@ import { API_URL, getFullName } from '../consts'
 const Home = () => {
     const [user, setUsers] = useState([])
     const [resNum, setResNum] = useState(10)
+    const [resGender, setResGender] = useState("")
+    const [resCountry, setResCountry] = useState("");
 
 
 
     const searchData = () => {
-        fetch(`${API_URL}?results=${resNum}`)
+        fetch(`${API_URL}?results=${resNum}&gender=${resGender}&nat=${resCountry}`)
             .then(res => res.json())
             .then(res => setUsers(res.results))
     }
+
+
+
+
     useEffect(() => {
-        searchData();
-    }, [])
+        const makeApiCall = setTimeout(searchData, 2000);
+
+
+        return () => clearTimeout(makeApiCall)
+
+    }, [resNum, resGender, resCountry])
 
     return (
-        <div className='w-full h-screen overflow-x-hidden overflow-y-auto bg-purple-700 p-5 grid grid-cols-6 justify-start items-center gap-4'>
+        <div className='w-full h-screen overflow-x-hidden overflow-y-auto bg-purple-700 p-5 grid grid-cols-6 justify-start items-start gap-y-2'>
+
+
             <div className="w-full h-16 bg-teal-500 col-span-6 rounded-md sticky top-0 z-10 p-3 flex justify-start items-center gap-x-4">
                 <input
                     onChange={(e) => setResNum(e.target.value)}
-                    type="number" name="user_number" className='px-3 py-2 rounded bg-gray-100 text-sm outline-none' placeholder='eg. 30' />
-                <button onClick={searchData} className='bg-blue-500 text-white px-3 py-1 shadow-md rounded'>Show</button>
+                    type="number" name="user_number"
+                    className='px-3 py-2 rounded bg-gray-100 text-sm outline-none'
+                    placeholder='eg. 30'
+                />
+
+                <select name="user_gender"
+                    onChange={(e) => setResGender(e.target.value)}
+                    className='px-3 py-2 rounded bg-gray-100 text-sm outline-none'
+                    id="user_gender">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="">All</option>
+                </select>
+
+
+
+                <select name="user_nat"
+                    onChange={(e) => setResCountry(e.target.value)}
+                    className='px-3 py-2 rounded bg-gray-100 text-sm outline-none'
+                    id="user_nat">
+                    <option value="in">India</option>
+                    <option value="au">Australia</option>
+                    <option value="us">USA</option>
+                    <option value="gb">England</option>
+                </select>
+
+
+
+
+
+                {/* <button onClick={searchData} className='bg-blue-500 text-white px-3 py-1 shadow-md rounded'>Show</button> */}
             </div>
+
+
+
             {
                 user.map((user, index) => (
-                    <UserCard key={index} name={getFullName(...Object.values(user.name))} image={user.picture.large} />
+                    <UserCard key={index} user={user} />
                 ))
 
             }
